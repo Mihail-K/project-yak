083a62ce3b3fa476ab86033f96f229cbaa69c8da
Minty Fresh <mintiewute@gmail.com>
2019-10-22 23:05:47 -0400

    Move logic into inline function.

--- src/include/assert.h
+++ src/include/assert.h
@@ -4,12 +4,16 @@
 #include "dsl.h"
 #include "logger.h"
 
-#define assert(condition) \
-    if (!(condition)) \
-    { \
-        fatal("Assertion failed: %s\n", #condition); \
-        fatal("From %s() on %s:%d\n", __func__, __FILE__, __LINE__); \
-        hang(); \
+#define assert(condition) _assert(condition, #condition, __func__, __FILE__, __LINE__)
+
+static inline void _assert(int result, const char* condition, const char* function, const char* filename, unsigned line)
+{
+    if (!result)
+    {
+        fatal("Assertion failed: %s\n", condition);
+        fatal("From %s() on %s:%d\n", __func__, __FILE__, __LINE__);
+        hang();
     }
+}
 
 #endif
