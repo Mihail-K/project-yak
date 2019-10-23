900472a86bf6b86017d6fe66f4929391735bb0fc
Minty Fresh <mintiewute@gmail.com>
2019-10-22 22:32:17 -0400

    Remove generated file from repo.

--- src/arch/i686/isr.c.sh
+++ src/arch/i686/isr.c.sh
@@ -1,6 +1,7 @@
 echo "// GENERATED FILE. DO NOT EDIT."
 echo "extern void isr_common_handler(unsigned interrupt, void* frame);"
 
+# Generate ISR handlers.
 for interrupt in `seq 0 255`;
 do
     echo "__attribute__((interrupt))"
@@ -10,6 +11,7 @@ do
     echo "}"
 done
 
+# Generate an array of ISR pointers.
 echo "void* isr_handlers[256] = {";
 for interrupt in `seq 0 255`;
 do
